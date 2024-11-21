bdca9ed03f4d1447d803804471397e536c5a7d40
lasal dissanayake <122669223+LasalDissanayake@users.noreply.github.com>
2024-11-22 00:53:26 +0530

    Text area tests

--- SeleniumLearning/src/test/java/TeaxtBoxExample.java
+++ SeleniumLearning/src/test/java/TeaxtBoxExample.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,10 +19,33 @@ public class TeaxtBoxExample {
         @Test
         public void testTextBox(){
             //Type Your Name
+            WebElement name = driver.findElement(By.id("j_idt88:name"));
+            name.sendKeys("Lasal");
+
             //Append country to this city
+            WebElement append = driver.findElement(By.id("j_idt88:j_idt91"));
+            append.sendKeys("Bla");
+
+            //verify if the text box is disabled
+            boolean isEnable = driver.findElement(By.id("j_idt88:j_idt91")).isEnabled();
+
+
             //clear the typed text
+            WebElement clearPath = driver.findElement(By.xpath("//*[@id='j_idt88:j_idt95']"));
+
+            clearPath.clear();
+
+
             //retrieve the type text
+            WebElement retrieveText = driver.findElement(By.xpath("//*[@id='j_idt88:j_idt97']"));
+
+            String text = retrieveText.getAttribute("value");
+            System.out.println("retrieve the type text is :"+text);
+
+
             //type email and Tab. confirm control moved to next element
+            WebElement email = driver.findElement(By.xpath("//*[@id='j_idt88:j_idt99']"));
+            email.sendKeys("l@g.com"+Keys.TAB);
 
         }
 
